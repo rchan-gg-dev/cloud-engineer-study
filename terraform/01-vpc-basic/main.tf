38c4e09b9314b1f1009832d85abb5bb5205891ab
rchan-gg-dev <318015336+rchan-gg-dev@users.noreply.github.com>
2026-09-18 00:40:38 +0900

    TerraformでVPCネットワーク構成を作成

--- terraform/01-vpc-basic/main.tf
+++ terraform/01-vpc-basic/main.tf
@@ -18,3 +18,93 @@ resource "aws_vpc" "main" {
     Name = "terraform-study-vpc"
   }
 }
+
+resource "aws_subnet" "public_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "ap-southeast-2a"
+
+  tags = {
+    Name = "terraform-study-public-a"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.2.0/24"
+  availability_zone = "ap-southeast-2b"
+
+  tags = {
+    Name = "terraform-study-public-b"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.11.0/24"
+  availability_zone = "ap-southeast-2a"
+
+  tags = {
+    Name = "terraform-study-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.12.0/24"
+  availability_zone = "ap-southeast-2b"
+
+  tags = {
+    Name = "terraform-study-private-b"
+  }
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-study-igw"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-study-public-rt"
+  }
+}
+
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.main.id
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "terraform-study-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
+}
